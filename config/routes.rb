64158a0a97762b461d47e0bc05a614a33e07aa85
Carmen <carmenleander@yahoo.com>
2015-02-12 09:36:32 -0500

    first commit carmen styles

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :boxes
 
-  root 'boxes#chat'
+  root 'boxes#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
