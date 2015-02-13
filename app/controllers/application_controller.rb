317731e4acc41437d0fba852b785fedf07b973c2
Enide Dufresne <enide.dufresne@gmail.com>
2015-02-13 13:54:04 -0500

    Fix login glitch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :get_user
-  before_action :redirect_unless_loggedin
+  before_action :redirect_unless_loggedin, except: :new
 
 
   def get_user
